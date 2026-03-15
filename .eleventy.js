1da34fed7bebbae31c9f4a065b3b7b2b8821973b
Zhenya Marych <zhenya.marych@icloud.com>
2026-03-15 11:49:14 +0100

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
+
+  return {
+    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/",
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    templateFormats: ["md", "njk", "html"]
+  };
+};
---
+++ src/pages/example-page.md
@@ -0,0 +1,10 @@
+---
+title: Example page
+layout: layouts/page.njk
+---
+
+This is an example markdown page.
+
+You can use it for a note, a case study, or any other simple subpage.
+
+![Example image]({{ '/assets/L1005086.jpg' | url }})
